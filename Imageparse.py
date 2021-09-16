ab02485fbd23413c60083d03bb03c724ce644192
Robert <robmpoole@gmail.com>
2021-09-16 14:41:45 -0500

    deleteing incorrect readme file

--- Imageparse.py
+++
@@ -1,3 +0,0 @@
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
-print(pytesseract.image_to_string('C:\\Users\\robmp\\Pictures\\Saved Pictures\\image1.png'))
